1222c3ec2cabc62b596cbc62b6e5d453143a939d
Ilya Baryshev <baryshev@gmail.com>
2013-08-05 09:59:37 +0400

    Repackage as django-cked-fc for PyPI

--- setup.py
+++ setup.py
@@ -8,17 +8,17 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name='django-cked',
-    version='0.0.8',
-    author='Svyatoslav Bulbakha',
-    author_email='mail@ssbb.me',
-    description=('CKEditor and elFinder integration for Django Framework.'),
+    name='django-cked-fc',
+    version='0.1',
+    author='Future Colors (original by Svyatoslav Bulbakha)',
+    author_email='info@futurecolors.ru',
+    description='CKEditor and elFinder integration for Django Framework.',
     license='BSD',
     keywords='django, ckeditor, elfinder, wysiwyg, upload',
-    url='https://bitbucket.org/ssbb/django-cked',
+    url='https://github.com/futurecolors/django-cked',
     packages=find_packages(),
     long_description=read('README.rst'),
-    install_requires = [
+    install_requires=[
         'pytils'
     ],
 )
