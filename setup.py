04f7e87c8f4868218206fbee3c48b1ddcbf790d9
Ilya Baryshev <baryshev@gmail.com>
2013-08-12 12:46:35 +0400

    Removed elFinder php code

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ from setuptools import setup, find_packages
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
     name='django-cked-fc',
     version='0.1',
