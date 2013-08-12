a4e1a20525170f742fe49884ef3bacf577961d40
Ilya Baryshev <baryshev@gmail.com>
2013-08-12 13:06:54 +0400

    Static files were never included
    include_package_data=True

--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
 import os
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
-# Utility function to read the README file.
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 
 setup(
     name='django-cked-fc',
-    version='0.1',
+    version='0.1.1',
     author='Future Colors (original by Svyatoslav Bulbakha)',
     author_email='info@futurecolors.ru',
     description='CKEditor and elFinder integration for Django Framework.',
     license='BSD',
     keywords='django, ckeditor, elfinder, wysiwyg, upload',
     url='https://github.com/futurecolors/django-cked',
-    packages=find_packages(),
-    long_description=read('README.rst'),
+    packages=['cked'],
+    long_description=README,
+    include_package_data=True,
     install_requires=[
         'pytils'
     ],
